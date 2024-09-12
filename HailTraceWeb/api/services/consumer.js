65e3776cbc7a5e1b6105bb889c35b9dc22468ad5
Juanito <krazybean@gmail.com>
2024-09-12 05:44:00 -0500

    more debugging hell

--- HailTraceWeb/api/services/consumer.js
+++ HailTraceWeb/api/services/consumer.js
@@ -1,23 +1,33 @@
 const { Kafka } = require('kafkajs');
 const kafkaConfig = require('../kafkaConfig');
+const kafkaController = require('../controllers/kafkaController');
 
 const kafka = new Kafka(kafkaConfig.kafka);
 const consumer = kafka.consumer({ groupId: 'weather-ui-consumer-group' });
 
 async function startConsumer() {
-  await consumer.connect();
-  await consumer.subscribe({ topic: kafkaConfig.kafka.topic, fromBeginning: true });
+    console.log('Connecting to Kafka...');
+    await consumer.connect();
+    console.log('Connected to Kafka!');
 
-  const interval = 30 * 1000; // 30 seconds
+    console.log('Subscribing to topic...');
+    await consumer.subscribe({ topic: kafkaConfig.kafka.topic, fromBeginning: true });
+    console.log('Subscribed to topic!');
 
-  setInterval(async () => {
-    await consumer.run({
-      eachMessage: async ({ topic, partition, message }) => {
-        console.log('Received message: ', message.value);
-        // Process the message
-      },
-    });
-  }, interval);
-}
+    const interval = 30 * 1000; // 30 seconds
+  
+    setInterval(async () => {
+      await consumer.run({
+        eachMessage: {
+          topic: kafkaConfig.kafka.topic,
+          partition: 0,
+          callback: async ({ topic, partition, message }) => {
+            console.log('Received message: ', message.value);
+            kafkaController.consumeMessages(message.value)
+          },
+        },
+      });
+    }, interval);
+  }
 
 module.exports = { startConsumer };
